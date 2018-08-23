a5ec19561d54d3a2f454daa070245575cd1bf899
Daniel Schwarz <d.schwarz@euroimmun.de>
2018-08-23 10:10:32 +0200

    Add perf project

---
+++ TrackerDogPerf/PerfTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PerfTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EUROIMMUN AG")]
+[assembly: AssemblyProduct("PerfTest")]
+[assembly: AssemblyCopyright("Copyright © EUROIMMUN AG 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7c2d8f8b-e5c7-4447-81ab-ca73031a4c75")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TrackerDogPerf/PerfTest/UnitTest1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrackerDog;
+using TrackerDog.Configuration;
+
+namespace PerfTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private ITrackableObjectFactory _trackableObjectFactory;
+
+        public class Class0
+        {
+            public virtual Class1 Class1 { get; set; }
+        }
+
+        public class Class1
+        {
+            public virtual ISet<Class2> Class2s { get; set; }
+        }
+
+        public class Class2
+        {
+            public int Number { get; set; }
+        }
+        
+
+        [TestInitialize()]
+        public void Initialize()
+        {
+            IObjectChangeTrackingConfiguration config = ObjectChangeTracking.CreateConfiguration();
+            config.TrackThisTypeRecursive<Class0>();
+
+            _trackableObjectFactory = config.CreateTrackableObjectFactory();
+        }
+
+        [TestMethod]
+        [DataRow(1 /* number of repeats */, 10 /* number of list items */)]
+        public void PerfTest(int n, int m)
+        {
+            var class0 = new Class0
+            {
+                Class1 = new Class1
+                {
+                    Class2s = new HashSet<Class2>(
+                        Enumerable.Range(0, m).Select(i => new Class2{ Number = i })
+                        )
+                }
+            };
+
+            var proxyList = new List<Class0>();
+            for (int i = 0; i < n; ++i)
+            {
+                var proxy = _trackableObjectFactory.CreateFrom(class0);
+                proxyList.Add(proxy);
+            }
+
+            Assert.AreEqual( proxyList.Count, n);
+        }
+    }
+}
